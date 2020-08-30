79e978f211071e382d7c70b1502e78934da2890d
Mohsen Alizadeh <mohsen@alizadeh.us>
2020-08-30 16:03:15 +0430

    add some examples

---
+++ spec/file_1_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe "File 1" do
+  it "takes 1 seconds" do
+    sleep 1
+  end
+
+  it "takes 2 seconds" do
+    sleep 2
+  end
+end
---
+++ spec/file_2_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe "File 2" do
+  it "item 1" do
+  end
+
+  it "item 2" do
+  end
+end
